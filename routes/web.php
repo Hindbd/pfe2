0c6d7035030db797b7b62358caaf1a4b2839b0f3
Hindbd <hindbouhedda0@gmail.com>
2023-04-16 18:57:55 +0000

    >>

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/welcome', function () {
 Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', [App\Http\Controllers\dashboardController::class, 'index'])->name('dashboard');
+Route::get('/home', [App\Http\Controllers\dashboardController::class, 'index'])->name('home');
 Route::get('/profile',[App\Http\Controllers\ProfileController2::class, 'index']);
 Route::get('/timeline',[App\Http\Controllers\TimelineController::class, 'index']);
 Route::get('/calendar',[App\Http\Controllers\CalendarController::class, 'index']);
@@ -38,3 +38,6 @@ Route::get('/projects',[App\Http\Controllers\projectsController::class, 'index']
 Route::get('/project-add',[App\Http\Controllers\addProjectController::class, 'index']);
 Route::get('/teams',[App\Http\Controllers\TeamsController::class, 'index']);
 Route::get('/contacts',[App\Http\Controllers\ContactsController::class, 'index']);
+// Route::get('/dashboard', function () {
+//     return view('index2');
+// })->name('dashboard');
