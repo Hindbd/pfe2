2a870edb27d422e53255bd40591cbacdd7090f8d
Hindbd <hindbouhedda0@gmail.com>
2023-05-07 22:07:28 +0100

    .

--- app/Http/Controllers/portfolioController.php
+++ app/Http/Controllers/portfolioController.php
@@ -6,6 +6,7 @@ use App\Models\Events;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Pubs;
+use Illuminate\Support\Facades\DB;
 
 class portfolioController extends Controller
 {
@@ -27,12 +28,26 @@ class portfolioController extends Controller
         return view('pubsPortfolio', ['user'=>$user,'pub' => $pub]);
     }
     public function show($id)
-     {   
+    {   
         //$pub=Pubs::all();
-        $pub = pubs::find($id);
+        // $pub = pubs::find($id);
+        $pubs = DB::table('pubs')
+                        ->where('id',$id)
+                        ->get();
+        $pub = DB::table('affect_event_pub')->where('FK_pub', $id)->first();
         $user=Auth::user();
-        $event = Events::find($id);
-        return view('portfolio', ['user'=>$user, 'event' => $event,'pub' => $pub]);
+        // $event = Events::find($id);
+        $event = DB::table('affect_event_pub')->where('FK_event', $pub->id)->first();
+        // $event=$event->id;
+        if ($event) {
+            $event_id = $event->id;
+            $events=Events::find($event_id);
+
+            // rest of your code
+        } else {
+            // handle the case when the event with the given id is not found
+        }
+        return view('portfolio', ['user'=>$user, 'events' => $events,'pub' => $pub]);
     }
 
 }
